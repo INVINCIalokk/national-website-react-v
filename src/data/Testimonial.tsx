acd75bd07eabdec78b2efb97ea1716182b392378
Alok choudhary <alokc8502@gmail.com>
2024-03-17 22:36:35 +0530

    updates testimonials

--- src/data/Testimonial.tsx
+++ src/data/Testimonial.tsx
@@ -7,52 +7,52 @@ export interface Testimonial {
   
   export const testimonials: Testimonial[] = [
     { 
-      name: "Sarah Johnson", 
+      name: "Shruti Bhardwaj", 
       review: "This coaching institute helped me achieve my goals! The instructors were supportive and knowledgeable.", 
       profileLink: "https://avatar.iran.liara.run/public" 
     },
     { 
-      name: "Mike Thompson", 
+      name: "Akshay Sharma", 
       review: "The personalized study plan and regular assessments were invaluable. Highly recommend!", 
       profileLink: "https://avatar.iran.liara.run/public" 
     },
     { 
-      name: "Emily Davis", 
+      name: "Parul Patel", 
       review: "I was hesitant about online coaching, but this exceeded my expectations. Great value!",
       profileLink: "https://avatar.iran.liara.run/public"  
     },
     {
-      name: "John Williams",
+      name: "Rahul Singh",
       review: "The structured curriculum and practice tests made a huge difference in my preparation.",
       profileLink: "https://avatar.iran.liara.run/public"
     },
     {
-      name: "Sophia Rodriguez",
+      name: "Sneha Mishra",
       review: "I appreciated the flexibility of the online classes and the ability to get help whenever I needed it.",
       profileLink: "https://avatar.iran.liara.run/public"  
     },
     {
-      name: "David Miller",
+      name: "Ajay Jain",
       review: "The mock interviews were incredibly helpful. I felt much more confident going into the real thing.",
       profileLink: "https://avatar.iran.liara.run/public"  
     },
     {
-      name: "Olivia Brown",
+      name: "Payal Patel",
       review: "This institute not only helped me with my exams but also boosted my overall confidence.",
       profileLink: "https://avatar.iran.liara.run/public"  
     },
     {
-      name: "Noah Taylor",
+      name: "Rahul Kumar",
       review: "The instructors were truly invested in my success. I wouldn't be where I am without their guidance.",
       profileLink: "https://avatar.iran.liara.run/public" 
     },
     {
-      name: "Emma Martinez",
+      name: "Priya Singh",
       review: "The study materials were well-organized and easy to follow. I felt prepared for every topic.",
       profileLink: "https://avatar.iran.liara.run/public" 
     },
     {
-      name: "Lucas Anderson",
+      name: "Laxman Sharma",
       review: "The community of students within the institute was a great support system.",
       profileLink: "https://avatar.iran.liara.run/public" 
     }
